c87b8b59692ec9a802c29579ee84f3c427b4c8b8
jeubanks59 <joshua.eubanks@knights.ucf.edu>
2018-05-07 20:47:39 -0400

    Foolish error corrected

--- Programs.sh
+++ Programs.sh
@@ -2,4 +2,4 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3;
 sudo apt-get update;
 sudo apt-get install sublime-text-installer;
 sudo apt-get install r-base;
-sudo apt-get texlive-full;
+sudo apt-get install texlive-full;
